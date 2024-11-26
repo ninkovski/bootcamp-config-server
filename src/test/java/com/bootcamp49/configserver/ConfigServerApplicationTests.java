6dbccf4c25db7b2f9e6ecaf39acfd1bcb029a73f
Ninkovski Morales Janampa <nmoralej@emeal.nttdata.com>
2024-11-26 10:31:43 -0500

    first commit

---
+++ src/test/java/com/bootcamp49/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp49.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
